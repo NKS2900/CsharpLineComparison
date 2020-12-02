3cba1e33b01878364aa3d2bd68177662d88ef11c
unknown <nksayyad2900.com>
2020-12-02 10:57:45 +0530

    [NIJAM] Add:Compare Lines on length

--- LineComparisonApp/Program.cs
+++ LineComparisonApp/Program.cs
@@ -10,25 +10,32 @@ namespace LineComparisonApp
             double length1;
             double length2;
             Random Number = new Random();
-            double x1= Number.Next(0, 3);
-            double x2= Number.Next(0, 3);
-            double x3 = Number.Next(0, 3);
-            double x4 = Number.Next(0, 3);
-            double y1= Number.Next(0, 3);
-            double y2= Number.Next(0, 3);
-            double y3 = Number.Next(0, 3);
-            double y4 = Number.Next(0, 3);
+            double x1= Number.Next(0, 5);
+            double x2= Number.Next(0, 5);
+            double x3 = Number.Next(0, 5);
+            double x4 = Number.Next(0, 5);
+            double y1= Number.Next(0, 5);
+            double y2= Number.Next(0, 5);
+            double y3 = Number.Next(0, 5);
+            double y4 = Number.Next(0, 5);
             length1 = (Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1))));
             length2 = (Math.Sqrt(((x4 - x3) * (x4 - x3)) + ((y4 - y3) * (y4 - y3))));
             Console.WriteLine(" 1st Line Length is : " + length1);
             Console.WriteLine(" 2nd Line Length is : " + length2);
-            if (length1 == length2)
+            double line1 = length1;
+            double line2 = length2;
+            if (line1 == line2)
             {
-                Console.WriteLine("Lines are Equal");
+                Console.WriteLine("Both Lines are Equal.");
             }
             else
             {
-                Console.WriteLine("Line is Not Equal");
+                int compare = line1.CompareTo(line2);
+                if (compare > 0)
+                    Console.WriteLine("Line 1 is larger");
+
+                else
+                    Console.WriteLine("Line 2 is larger");
             }
         }
     }
