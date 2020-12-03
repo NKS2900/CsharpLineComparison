bbc29854e33dbbb8cde414f0b16a143b21c5c938
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-12-03 09:32:59 +0530

    [NIJAM] Add: Compare lines using CompareTo Method

--- LineComparisonApp/Program.cs
+++ LineComparisonApp/Program.cs
@@ -7,35 +7,35 @@ namespace LineComparisonApp
         static void Main(string[] args)
         {
             Console.WriteLine("************Welcome To Line Comparison*******");
+            Console.WriteLine("Welcome to Line comparison project !");
             double length1;
             double length2;
             Random Number = new Random();
-            double x1= Number.Next(0, 5);
-            double x2= Number.Next(0, 5);
+            double x1 = Number.Next(0, 5);
+            double x2 = Number.Next(0, 5);
+            double y1 = Number.Next(0, 5);
+            double y2 = Number.Next(0, 5);
             double x3 = Number.Next(0, 5);
             double x4 = Number.Next(0, 5);
-            double y1= Number.Next(0, 5);
-            double y2= Number.Next(0, 5);
             double y3 = Number.Next(0, 5);
             double y4 = Number.Next(0, 5);
-            length1 = (Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1))));
-            length2 = (Math.Sqrt(((x4 - x3) * (x4 - x3)) + ((y4 - y3) * (y4 - y3))));
+            length1 = (Math.Sqrt((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+            length2 = (Math.Sqrt((x4 - x3) * (x4 - x3)) + ((y4 - y3) * (y4 - y3)));           
             Console.WriteLine(" 1st Line Length is : " + length1);
             Console.WriteLine(" 2nd Line Length is : " + length2);
-            double line1 = length1;
-            double line2 = length2;
-            if (line1 == line2)
-            {
-                Console.WriteLine("Both Lines are Equal.");
-            }
-            else
-            {
-                int compare = line1.CompareTo(line2);
-                if (compare > 0)
-                    Console.WriteLine("Line 1 is larger");
 
-                else
-                    Console.WriteLine("Line 2 is larger");
+            if ((length1.CompareTo(length2)).Equals(-1))
+            {
+                Console.WriteLine("Line 1 is greater thean line 2");
+
+            }
+            else if ((length1.CompareTo(length2)).Equals(1))
+            {
+                Console.WriteLine("Line 1 is greater thean line 2");
+            }
+            else if ((length1.CompareTo(length2)).Equals(0))
+            {
+                Console.WriteLine("Both Lines are equal");
             }
         }
     }
